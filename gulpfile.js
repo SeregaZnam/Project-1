90bded71e592161688ba358c1ded9213189b1a25
SeregaZnam <Sergey-znamensk@yandex.ru>
2017-10-06 01:10:44 +0400

    step1

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,17 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
-var connect = require('gulp-connect');
-var replace = require('gulp-html-replace');
-var includer = require('gulp-htmlincluder');
-var livereload = require('gulp-livereload');
+var gulp 		  = require('gulp');
+var concatCss 	  = require('gulp-concat-css');
+var connect 	  = require('gulp-connect');
+var replace 	  = require('gulp-html-replace');
+var includer      = require('gulp-htmlincluder');
+var livereload 	  = require('gulp-livereload');
 var spritecreator = require('gulp.spritesmith');
-
-var less = require('gulp-less');
-var path = require('path');
+var less 		  = require('gulp-less');
  
 gulp.task('less', function () {
   return gulp.src('dev/styles/*.less')
     .pipe(less())
     .pipe(gulp.dest('build'))
-});
-
-gulp.task('sprite', function(){
-	var spriteData = gulp.src('dev/img/icons/*.png')
-						.pipe(spritecreator({
-							imgName: 'sprite.png',
-							cssName: 'sprite.css',
-							algorithm: 'binary-tree'
-						}));
-	spriteData.img.pipe(gulp.dest('build/img/'));
-	spriteData.css.pipe(gulp.dest('build/css/'));
+    .pipe(connect.reload());
 });
 
 gulp.task('server', function(){
@@ -31,12 +19,6 @@ gulp.task('server', function(){
 		root : 'build',
 		livereload: true
 	});
-});
-gulp.task('css', function(){
-	gulp.src('dev/css/**/*.css')
-		.pipe(concatCss('style.css'))
-		.pipe(gulp.dest('build/style/'))
-		.pipe(connect.reload());
 });
 
 gulp.task('html', function(){
