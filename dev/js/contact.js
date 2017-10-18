4a50894ba6dc03b4257be06d87b3bcddf0eda962
SeregaZnam <Sergey-znamensk@yandex.ru>
2017-10-19 00:43:09 +0400

    step 14

---
+++ dev/js/contact.js
@@ -0,0 +1,34 @@
+var submit     = document.getElementById('submit');
+var formInfo   = document.forms['form_info'];
+var inputName  = formInfo.elements.name;
+var inputEmail = formInfo.elements.email;
+var message    = formInfo.elements.message;
+var reg        = /^[a-zA-z]+\W?[a-z]+@[a-zA-z]+\.[a-z]{2,3}$/;
+var formInfo   = document.forms['form_info'];
+var flag 	   = false;
+
+submit.addEventListener('click', function(event){
+	
+	if(inputName.length < 3 || inputName.value == '') {
+		inputName.style.outline = '1px solid red';
+	}
+
+	if(!reg.test(inputEmail.value)) {
+		inputEmail.style.outline = '1px solid red';
+	}
+
+	if(message.value == '') {
+		message.style.outline = '1px solid red';
+	}
+
+	if(inputName.length < 3 || !reg.test(inputEmail.value) || message.value == '') {
+		if(flag){
+			event.preventDefault();
+			return;
+		} else {
+			document.getElementById('test').insertAdjacentHTML('afterBegin', '<div class="alert alert-danger">Поля выделенные красным заполнены некорректно</div>');
+			flag = true;
+			event.preventDefault();
+		}
+	}
+});
